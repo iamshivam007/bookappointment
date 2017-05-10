3f2aeacdcea69c95f5e5dc266b53d401c053fd72
shivji <jindalshivam65@gmail.com>
2017-05-11 00:26:21 +0530

    Added fonts

---
+++ static/l10n/en.js
@@ -0,0 +1,27 @@
+{
+  "header": {
+  "navbar" : {
+    "UPLOAD" : "Upload",
+      "new" : {
+      "NEW" : "New",
+        "PROJECT" : "Projects",
+        "TASK" : "Task",
+        "USER" : "User",
+        "EMAIL" : "Email"
+    }
+  }
+},
+  "aside" : {
+  "nav" : {
+    "HEADER" : "Navigation",
+      "DASHBOARD" : "Dashboard",
+      "CLASSES" : "Classes",
+      "SUBJECTS" : "Subjects",
+      "BOOKS" : "Books",
+      "CHAPTERS" : "Chapters",
+      "QUESTIONS" : "Questions",
+      "AUTHORS" : "Authors",
+      "GENERATEPAPER" : "Generate Paper"
+  }
+}
+}
