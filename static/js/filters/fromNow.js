e3cc7d436bc2a47615598851425482a3c8fb3703
shivji <jindalshivam65@gmail.com>
2017-05-13 23:44:29 +0530

    Added time Filter

--- static/js/filters/fromNow.js
+++ static/js/filters/fromNow.js
@@ -7,3 +7,14 @@ app.filter('fromNow', function() {
     }
   }
 );
+
+app.filter('timeFilter', function ($filter) {
+  return function (time) {
+    if (!Date.prototype.isPrototypeOf(time)){
+      var parts = time.split(':');
+      var date = new Date(0, 0, 0, parts[0], parts[1], parts[2]);
+      return $filter('date')(date, 'H:mm');
+    }
+    return $filter('date')(time, 'H:mm');
+  };
+});
