004839be779b82cdaeac36517f12e7d9212686bf
shivji <jindalshivam65@gmail.com>
2017-05-21 00:39:38 +0530

    Added detail field in RoleSubscriptionSerializer

--- apps/api/serializers/assistent.py
+++ apps/api/serializers/assistent.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 
 from apps.core.models.assistent import *
 from utils.utils import Base64ImageField
+from apps.api.serializers import RoleSerializer, UserSerializer
 
 
 class PersonalAssistantSerializer(serializers.ModelSerializer):
+    user_detail = serializers.SerializerMethodField()
     image = Base64ImageField(required=False)
 
     class Meta:
         model = PersonalAssistant
+
+    def get_user_detail(self, personal_assistant):
+        return UserSerializer(personal_assistant.user).data
 
 
 class ServiceSubscriptionSerializer(serializers.ModelSerializer):
@@ -18,9 +23,17 @@ class ServiceSubscriptionSerializer(serializers.ModelSerializer):
 
 
 class RoleSubscriptionSerializer(serializers.ModelSerializer):
+    personal_assistant_detail = serializers.SerializerMethodField()
+    role_detail = serializers.SerializerMethodField()
 
     class Meta:
         model = RoleSubscription
+
+    def get_personal_assistant_detail(self, role_subscription):
+        return PersonalAssistantSerializer(role_subscription.personal_assistant).data
+
+    def get_role_detail(self, role_subscription):
+        return RoleSerializer(role_subscription.role).data
 
 
 class StoreSubscriptionSerializer(serializers.ModelSerializer):
