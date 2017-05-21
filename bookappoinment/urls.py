d4fdce64e42d806a9a19aae20f436c2737b3980d
shivji <jindalshivam65@gmail.com>
2017-05-21 23:00:00 +0530

    Added media urls

--- bookappoinment/urls.py
+++ bookappoinment/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
 
 from utils.views import IndexView, TPLView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -30,4 +33,10 @@ urlpatterns += [
     url(r'^$', IndexView.as_view()),
     url(r'^tpl/(?P<tpl_name>.*)', TPLView.as_view()),
     url(r'^', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [
+    url(
+        r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}
+    ),
 ]
